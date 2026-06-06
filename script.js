428bb99ed5ef707233e572bb0c024993fda10e8c
Mohitxy0092 <mohit952096@gmail.com>
2026-06-06 23:37:26 +0530

    Add dark mode toggle

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const clock = document.getElementById("clock");
+
 const updateClock = () => {
   clock.textContent = new Date().toLocaleTimeString();
 };
+
 updateClock();
 setInterval(updateClock, 1000);
+
+const themeBtn = document.getElementById("theme-btn");
+
+if (themeBtn) {
+  themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+
+    localStorage.setItem(
+      "theme",
+      document.body.classList.contains("light") ? "light" : "dark",
+    );
+  });
+}
+
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light");
+}
